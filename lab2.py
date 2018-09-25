2b013ef2f932de5ee5a67210785d87cb1a90819e
Oskar Olofsson <oskarolo@kth.se>
2018-09-25 18:12:25 +0200

    objective

--- lab2.py
+++ lab2.py
@@ -8,6 +8,9 @@ N = 27
 
 vector = numpy.zeros(N)
 
+i, j = 5, 5;
+precalculated = [[0 for l in range(0, j)] for k in range(0, i)]
+
 C = 1
 
 bounds=[(0, C) for b in range(N)]
@@ -20,15 +23,21 @@ bounds=[(0, C) for b in range(N)]
 
 #pij_array = pij(0, 0, 0, 0, 0)
 
+def precalculate(t, x, kernel):
+    for i in range(0, len(precalculated)):
+        for j in range(0, len(precalculated[0])):
+            precalculated[i][j] = t[i]*t[j]*kernel(x, x)
+
+
 def pij(t, i, j, x, y):
     t[i]*t[j]*linear_kernel(x, y)
 
 #K(⃗x, ⃗y ) = (⃗x T · ⃗y + 1) p
 
-def radial_basis_function_kernel(x, y, sigma):
+def radial_basis_function_kernel(x, y, sigma = 1):
     return math.exp(-((distance.euclidean(x, y)**2)/(2*sigma*sigma)))
 
-def polynomial_kernel(x, y, p):
+def polynomial_kernel(x, y, p = 3):
     return (numpy.dot(numpy.transpose(x), y) + 1)**p
 
 def linear_kernel(x, y):
@@ -39,7 +48,15 @@ def linear_kernel(x, y):
 
 def objective(a):
     #0.5*
-    return numpy.sum(numpy.sum(a, 0), 0)
+    sum1 = 0
+    sum2 = 0
+    for i in range(0, len(precalculated)):
+        sum2 += a[i]
+        for j in range(0, len(precalculated[0])):
+            sum1 += a[i]*a[j]*precalculated[i][j]
+
+    return 0.5*sum1 - sum2
+
     #numpy.sum(a, axis=None, dtype=None, out=None, keepdims=<no value>, initial=<no value>)[source]
 
 def zerofun(a, t, C):
@@ -48,11 +65,11 @@ def zerofun(a, t, C):
             print("out of bounds in zerofun")
             return "out of bounds error"
 
-    thesum = 0
+    sum = 0
     for i in range(0, len(a)):
-        thesum = thesum + (a[i] * t[i])
+        sum = sum + (a[i] * t[i])
 
-    return thesum
+    return sum
 
 
 
@@ -65,5 +82,12 @@ def main():
    #print(objective([0,7,11], [0, 83, 2]))
    print("rbf kern: ", radial_basis_function_kernel([3, 0, 4, 2], [2, 0 ,1, 9], 1))
    print("zerofun: ", zerofun([0,2,7], [1, -1 , 1], 10))
+
+   print("before: ", precalculated)
+   precalculate([1, -1, 1, 1, 1], [4, 3, 7, 8, 5], linear_kernel)
+   print("william: ", precalculated)
+
+   print(objective([1, 4, 3, 3, 7]))
+
 
 main()
